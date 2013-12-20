73932da197a73ff65b6fa4d095cef785e997165b
+My <kaypav@gmail.com>
2013-12-19 18:01:41 -0700

    add Random() and Random(k)

--- Listless/Assets/Scripts/Listless.cs
+++ Listless/Assets/Scripts/Listless.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections.Generic;
 using System;
+using UnityEngine;
 
 namespace Listless
 {
@@ -15,6 +16,44 @@ namespace Listless
             T temp = list[a];
             list[a] = list[b];
             list[b] = temp;
+        }
+
+        public static T Random<T>(this List<T> list)
+        {
+            return list.Random(1)[0];
+        }
+
+        public static List<T> Random<T>(this List<T> list, int count)
+        {
+            if (count > list.Count)
+            {
+                throw new IndexOutOfRangeException();
+            }
+
+            List<int> chosenIndexes = new List<int>();
+            List<T> chosenItems = new List<T>();
+
+            for (int i = 0; i < count; i++)
+            {
+                int chosenIndex = UnityEngine.Random.Range(0, list.Count);;
+
+                while (chosenIndexes.Contains(chosenIndex))
+                {
+                    chosenIndex = UnityEngine.Random.Range(0, list.Count);
+                } 
+
+                chosenIndexes.Add(chosenIndex);
+                chosenItems.Add(list[chosenIndex]);
+            }
+
+            return chosenItems;
+        }
+
+        public static List<T> RandomWithReplacement<T>(this List<T> list, int count)
+        {
+            List<T> chosenItems = new List<T>();
+            chosenItems.Add(list.Random());
+            return chosenItems;
         }
     }
 }
--- Listless/Assets/Tests/ListlessTest.cs
+++ Listless/Assets/Tests/ListlessTest.cs
@@ -7,6 +7,10 @@ using Listless;
 public class ListlessTest : TestBehaviour
 {
     List<int> it;
+    int chosen;
+    List<int> chosens;
+    Dictionary<int, int> histogram;
+
     public override void Spec()
     {
         Scenario("uses Listless extension methods");
@@ -24,40 +28,60 @@ public class ListlessTest : TestBehaviour
 
         Given("it contains numbers 1 through 5")
             .When("a random item is chosen 1000 times")
-            .Then("no item should be chosen more than 25 percent of the time")
-            .And("no item should be chosen less than 15 percent of the time")
+            .Then("no item should be chosen more than 250 times")
+            .And("no item should be chosen less than 150 times")
             .Because("Random() should return fairly random results");
 
         Given("it contains numbers 1 through 5")
-            .When("3 item are chosen")
-            .Then("all 3 items should be contained in the list")
+            .When("3 random items are chosen")
+            .Then("it should return 3 items")
+            .And("it should contain all the items")
             .Because("Random(k) should return k items contained in the list");
 
         Given("it contains numbers 1 through 5")
-            .When("3 items are chosen")
+            .When("3 random items are chosen")
             .Then("none of the items should be duplicates")
             .Because("Random(k) should never return the same item twice");
 
         Given("it contains numbers 1 through 5")
             .When("3 random items are chosen 1000 times")
-            .Then("no item should be chosen more than 25 percent of the time")
-            .And("no item should be chosen less than 15 percent of the time")
+            .Then("no item should be chosen more than 750 times")
+            .And("no item should be chosen less than 450 times")
             .Because("Random(k) should return fairly random results");
 
         Given("it contains numbers 1 through 5")
+            .When("3 random items are chosen with replacement")
+            .Then("it should return 3 items")
+            .And("it should contain all the items")
+            .Because("RandomWithReplacement(k) should return k items contained in the list");
+
+        Given("it contains numbers 1 through 5")
+            .When("100 random items are chosen with replacement")
+            .Then("it should return 100 items")
+            .Because("RandomWithReplacement(k) can return duplicates");
+
+        Given("it contains numbers 1 through 5")
+            .When("3 random items are chosen 1000 times with replacement")
+            .Then("no item should be chosen more than 750 times")
+            .And("no item should be chosen less than 450 times")
+            .Because("RandomWithReplacement(k) should return fairly random results");
+
+        Given("it contains numbers 1 through 5")
             .When("it is shuffled 1000 times")
-            .Then("no item should be in a position more than 25 percent of the time")
-            .And("no item should be in a position less than 15 percent of the time")
+            .Then("no item should be in a position more than 250 times")
+            .And("no item should be in a position less than 150 times")
             .Because("shuffle should be pretty random");
     }
 
     public void ItContainsNumbers__Through__(int start, int finish)
     {
         it = new List<int>();
+        histogram = new Dictionary<int, int>();
 
         for (int i = start; i <= finish; i++)
         {
             it.Add(i);
+            histogram[i] = 0;
         }
     }
 
@@ -66,8 +90,87 @@ public class ListlessTest : TestBehaviour
         it.Swap(a, b);
     }
 
+    public void ARandomItemIsChosen()
+    {
+        chosen = it.Random();
+    }
+
+    public void __RandomItemsAreChosen(int count)
+    {
+        chosens = it.Random(count);
+    }
+
+    public void ARandomItemIsChosen__Times(int times)
+    {
+        for (int i = 0; i < times; i++)
+        {
+            histogram[it.Random()]++;
+        }
+    }
+
+    public void __RandomItemsAreChosen__Times(int count, int times)
+    {
+        for (int i = 0; i < times; i++)
+        {
+            foreach (int item in it.Random(count))
+            {
+                histogram[item]++;
+            }
+        }
+    }
+
     public void Index__ShouldBe__(int index, int expected)
     {
         it[index].ShouldBe(expected);
+    }
+
+    public void ItShouldContainTheItem()
+    {
+        it.ShouldContain(chosen);
+    }
+
+    public void NoItemShouldBeChosenMoreThan__Times(int times)
+    {
+        foreach (int count in histogram.Values)
+        {
+            count.ShouldBeLessThan(times);
+        }
+    }
+
+    public void NoItemShouldBeChosenLessThan__Times(int times)
+    {
+        foreach (int count in histogram.Values)
+        {
+            count.ShouldBeGreaterThan(times);
+        }
+    }
+
+    public void ItShouldReturn__Items(int count)
+    {
+        chosens.Count.ShouldBe(count);
+    }
+
+    public void ItShouldContainAllTheItems()
+    {
+        foreach (int chosen in chosens)
+        {
+            it.ShouldContain(chosen);
+        }
+    }
+
+    public void NoneOfTheItemsShouldBeDuplicates()
+    {
+        for (int i = 0; i < chosens.Count; i++)
+        {
+            for (int j = 0; j < chosens.Count; j++)
+            {
+                if (i == j)
+                {
+                    continue;
+                }
+
+                i.ShouldNotBe(j);
+            }
+        }
     }
 }
